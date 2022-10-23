be9e4b001f86e2972d00e71476d15493507452d1
msham3a <mshamaa2013@gmail.com>
2022-10-23 17:49:58 +0200

    delete modules folder

--- starter/src/components/BookShelfChanger.js
+++ starter/src/components/BookShelfChanger.js
@@ -2,7 +2,8 @@ function BookShelfChanger({ book }){
 
   const options=[{ id: 1, name: "Currently Reading" ,value:"currentlyReading"},
   { id: 2, name: "Want to Read" ,value:"wantToRead"},
-  { id: 3, name: "Read" ,value:"read"},]
+  { id: 3, name: "Read" ,value:"read"},
+  { id: 4, name: "None" ,value:"none"},]
   
   const optionsList=options.filter((op)=> book.shelf!==op.value).map((op)=> (
     <option key={op.id} value={op.value}>{op.name}</option>
